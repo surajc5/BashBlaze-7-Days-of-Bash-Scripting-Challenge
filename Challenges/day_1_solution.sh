5a5ab78b33bf1591aa1ea641eb306c8c0dc6b730
surajc5 <surajbarik20@gmail.com>
2023-07-31 11:29:30 +0000

    First day solution

---
+++ Challenges/day_1_solution.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# First line of the script is the shebang which tells the system how to execute
+
+# Task-2
+echo " #TWSBashBlazeChallenge lets rock "
+
+# Task -3
+variable1=12
+variable2=15
+
+#Task -4
+variable=$((variable1+variable2))
+echo "The summation of two number is $variable"
+
+
+#Task -5
+echo "where am I currently is $(pwd)"
+echo "list of directory is $(ls) "
+echo " displays the login name is $(whoami) "
+
+#Task -6
+echo "Files with .txt extension in the current directory"
+ls *.sh
+
+
